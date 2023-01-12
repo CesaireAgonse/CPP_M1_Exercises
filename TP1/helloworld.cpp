38e898c7a3bf490aed2725b3a8d7e1b8d86fc842
Cesaire Agonce <cesaire.agonse@edu.univ-eiffel>
2023-01-12 15:21:09 +0100

    fin du tp

--- TP1/helloworld.cpp
+++ TP1/helloworld.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+using namespace std;
+
 int main()
 {
-
+    // Q1
+    /*
     std::cout << "Hello World!" << std::endl;
+    */
+
+    // Q2
+    /*
+    int var;
+    std::cin >> var;
+    */
+
+    // Q3
+    string var;
+    cout << "What your name ?\n";
+    cin >> var;
+    cout << "Hello " + var + "!" << endl;
 
     return 0;
 }
