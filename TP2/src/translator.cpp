c9b06e7b68c79c28268cf60a20f53d2ba42a7641
Cesaire Agonce <cesaire.agonse@edu.univ-eiffel>
2023-01-17 11:55:35 +0100

    exo1

--- TP2/src/translator.cpp
+++ TP2/src/translator.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
 
 int main()
 {
-    while (true)
-    {
-        std::cout << "This is a translator, but it does nothing for now..." << std::endl;
-        std::cin.ignore();
-    }
 
+    // base
+    /*
+    std::cout << "This is a translator, but it does nothing for now..." << std::endl;
+    std::cin.ignore();
+    */
+
+    string input = "";
+    set<string> possibilities = {"q",
+                                 "e",
+                                 "quit",
+                                 "exit"};
+    while (!possibilities.count(input))
+    {
+        cout << "This is a translator, but it does nothing for now..." << endl;
+        cin >> input;
+    }
+    cout << "Sortie du programme" << endl;
     return 0;
 }
